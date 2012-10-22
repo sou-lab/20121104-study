777863441d2b949d4a3ca5878343ced2a849e699
tomoki <tomoki@marin-no-MacBook-Air.local>
2012-10-22 22:42:45 +0900

    sass and Zen-Coding Snippets

---
+++ sass-bat/minify.bat
@@ -0,0 +1,8 @@
+﻿cd /d %~dp0
+:: 現在のディレクトリに移動
+
+sass --style compressed --watch sass:css --cache-location sass/.sass-cache
+:: 【--style】CSSのフォーマット
+:: 【--watch】scssファイルの自動監視
+:: 【sass:html/css】sassファイルの場所:cssファイルの書き出し先
+:: 【--cache-location】キャッシュファイルの保存先
